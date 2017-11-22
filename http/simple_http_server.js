53491bd03626379dd640040655e6834d23d9e244
gaozhidong <1011284295@qq.com>
2017-11-22 17:56:04 +0800

    simple node server

---
+++ http/simple_http_server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer();
+server.listen(8808)
+
+server.on('request',(req,res) =>{
+  res.statusCode = 200;
+  res.end('welcome to my first http server');
+});
