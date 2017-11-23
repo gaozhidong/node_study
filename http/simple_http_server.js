cee7029409b5aba0cd68ab6a8d7cfbd3af243293
gaozhidong <1011284295@qq.com>
2017-11-23 14:45:52 +0800

    resquest url

--- http/simple_http_server.js
+++ http/simple_http_server.js
@@ -1,8 +1,38 @@
+//Uniform Resource Locator
+// Schema://host:port/path?query#hash  //Schema 协议名    host：主机名  prot：端口号  
+
+// port: 22 ssh, 80:http  443:https  27017:mongodb
+
+
+// const http = require('http');
+// const server = http.createServer();
+// server.listen(8808)
+
+// server.on('request',(reqeust,response) =>{
+//   response.statusCode = 200;
+//   response.end('welcome to my first http server');
+// });
+
 const http = require('http');
 const server = http.createServer();
 server.listen(8808)
 
-server.on('request',(req,res) =>{
-  res.statusCode = 200;
-  res.end('welcome to my first http server');
+server.on('request',(request,response) =>{
+
+  const url = request.url;
+
+  console.log(url);
+
+  let responseStr;
+
+  if(url === '/hello'){
+    responseStr = 'hi there';
+  }else if(url === '/bye'){
+    responseStr = 'see ya next time';
+  }else{
+    responseStr = 'i cant understand what you are saying'
+  }
+
+  response.statusCode = 200;
+  response.end(responseStr);
 });
