9c6784385b12fb288141c629d1b982872f06fc60
gaozhidong <1011284295@qq.com>
2017-12-14 15:53:28 +0800

    async

--- async/async_demo.js
+++ async/async_demo.js
@@ -1,20 +1,64 @@
 
 
-function foo(params, callback) {
-  setTimeout(() => {
-    callback(new Error('oops'), 'hello')
-  }, 1000);
-}
+// function foo(params, callback) {
+//   setTimeout(() => {
+//     callback(new Error('oops'), 'hello')
+//   }, 1000);
+// }
 
-foo({}, (err, result) => {
-  if (err) {
-    console.log(err.stack)
-  } else {
-    console.log(result)
-  }
-  bar({}, () => {
-    zoo({}, () => {
+// foo({}, (err, result) => {
+//   if (err) {
+//     console.log(err.stack)
+//   } else {
+//     console.log(result)
+//   }
+//   bar({}, () => {
+//     zoo({}, () => {
 
+//     })
+//   })
+// })
+
+
+//解决方案： Promise + async/await
+
+
+// 同步代码
+// function dealWithObj(){
+//   obj.dealt = true
+// }
+
+// const obj = {}
+
+// console.log(obj)
+
+// dealWithObj(obj)
+
+// console.log(obj)
+
+
+(async () => {
+  async function dealWithObjAsync(obj) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        obj.dealt = true
+        resolve()
+      }, 2000);
     })
+
+  }
+  const obj = {}
+
+  console.log(obj)
+
+  await dealWithObjAsync(obj)
+
+  console.log(obj)
+
+})()
+  .then(r => {
+
   })
-})
+  .catch(e => {
+
+  })
