1c487ccca6d1822921a4968efd33208d04c09f2d
gaozhidong <1011284295@qq.com>
2017-12-14 16:16:52 +0800

    Promise

--- async/async_demo.js
+++ async/async_demo.js
@@ -37,28 +37,62 @@
 // console.log(obj)
 
 
-(async () => {
-  async function dealWithObjAsync(obj) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        obj.dealt = true
-        resolve()
-      }, 2000);
-    })
+// (async () => {
+//   async function dealWithObjAsync(obj) {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         obj.dealt = true
+//         resolve()
+//       }, 2000);
+//     })
 
-  }
-  const obj = {}
+//   }
+//   const obj = {}
 
-  console.log(obj)
+//   console.log(obj)
 
-  await dealWithObjAsync(obj)
+//   await dealWithObjAsync(obj)
 
-  console.log(obj)
+//   console.log(obj)
 
-})()
-  .then(r => {
+// })()
+//   .then(r => {
 
-  })
+//   })
+//   .catch(e => {
+
+//   })
+
+
+// function dealWithObj(obj, cb) {
+//   setTimeout(() => {
+//     obj.dealt = true
+//     cb(null, obj)
+//   }, 2000);
+// }
+
+// const obj = {}
+
+// console.log(obj)
+
+// dealWithObj(obj, (err) => {
+//   console.log(obj)
+// })
+
+
+//pending ==》 resolved/rejected
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log(1)
+    resolve('done')
+  }, 2000);
+})
+
+console.log(`promise${p}`)
+
+p.then((r) => {
+  console.log(`result:${r}`)
+})
   .catch(e => {
 
   })
