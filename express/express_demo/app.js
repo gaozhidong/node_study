094445be4fdad7a068a3b4cf479738bafb5131f0
gaozhidong <1011284295@qq.com>
2018-01-11 10:01:20 +0800

    error login ...

--- express/express_demo/app.js
+++ express/express_demo/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const errorHandler = require('./middlewares/http_error_handler');
+const logger = require('./utils/loggers/logger');
+require('./services/mongodb_connection');
+const NotFoundError = require('./errors/http_errors/resource_not_found_error');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const apiIndex = require('./routes/api');
 
 const app = express();
 
@@ -14,34 +15,31 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/user', users);
+app.use('/api', apiIndex);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+  if (!res.headersSent) {
+    next(new NotFoundError(req.method, req.path, '没有找到您要的资源哦'));
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(errorHandler());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+process.on('uncaughtException', (err) => {
+  logger.error('uncaught exception', { err });
+});
+
+process.on('unhandledReject', (reason, p) => {
+  logger.error('unhandledRejection', { reason, p });
 });
 
 module.exports = app;
